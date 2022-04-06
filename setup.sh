8819951592c25ba8de734aef76b67dad73e7fb9c
chinmay29hub <chinmaysonawane57@gmail.com>
2022-04-06 18:50:54 +0530

    corrected sh file

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-mkdir -p ~/.streamlit/echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/
+echo "[general]  
+email = \"me@gmail.com\""  > ~/.streamlit/credentials.toml
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false"  >> ~/.streamlit/config.toml
